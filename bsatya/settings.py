d7af1427dbe767648a8603fc2b1ae8a635a8132c
bkpl <i.am.bhandari.kapil@gmail.com>
2021-04-28 17:52:30 +0545

    configred middleware

--- bsatya/settings.py
+++ bsatya/settings.py
@@ -136,10 +136,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')

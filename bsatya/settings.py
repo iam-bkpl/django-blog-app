cd83a65eafd0d587704d964033cf22d0d5f0f06c
bkpl <i.am.bhandari.kapil@gmail.com>
2021-04-28 17:52:50 +0545

    configred middleware

--- bsatya/settings.py
+++ bsatya/settings.py
@@ -50,15 +50,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
-
-    # 'whitenoise.middleware.common.WhiteNoiseMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
 ]
 
 ROOT_URLCONF = 'bsatya.urls'
